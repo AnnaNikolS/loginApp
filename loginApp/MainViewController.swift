de607fbb5dfdf9465e9091521982b62e7f49c3ad
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-11 03:32:55 +0300

    creating classes

---
+++ loginApp/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  loginApp
+//
+//  Created by Анна on 11.02.2024.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ loginApp/WelcomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeViewController.swift
+//  loginApp
+//
+//  Created by Анна on 11.02.2024.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
