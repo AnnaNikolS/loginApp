c81927df9d6bf05087f46fbf9e45854de8c4ff43
AnnaNikolS <137338922+AnnaNikolS@users.noreply.github.com>
2024-02-11 22:10:47 +0300

    project logic

--- loginApp/MainViewController.swift
+++ loginApp/MainViewController.swift
@@ -7,10 +7,74 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
     
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var logInButton: UIButton!
+    
+    let userName = "Ann"
+    let password = "11"
+    
+    let gradientMainView = CAGradientLayer()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        gradientMainView.frame = view.bounds
+        gradientMainView.colors = [UIColor.systemIndigo.cgColor, UIColor.systemCyan.cgColor]
+        gradientMainView.locations = [0.0, 1.0]
+        view.layer.insertSublayer(gradientMainView, at: 0)
+        
+        userNameTextField.backgroundColor = .white
+        userNameTextField.attributedPlaceholder = NSAttributedString(string: "User Name", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        
+        passwordTextField.backgroundColor = .white
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "Password", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        
+        logInButton.layer.cornerRadius = 5
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeViewController = segue.destination as? WelcomeViewController
+        welcomeViewController?.user = userName
+        
+        welcomeViewController?.gradientWelcomeView.frame = view.bounds
+        welcomeViewController?.gradientWelcomeView.colors = [UIColor.red.cgColor, UIColor.systemOrange.cgColor]
+        welcomeViewController?.gradientWelcomeView.locations = [0.0, 1.0]
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        if (userNameTextField.text == userName) && (passwordTextField.text == password) {
+            return true
+        } else {
+            showAlert(withTitle: "Invalid login or password", andMessage: "Please, enter correct login and password")
+        }
+        return false
+    }
+    
+    @IBAction func pressedFUNButton() {
+        showAlert(withTitle: "Oops!", andMessage: "Your name is Ann 😉")
+    }
+    
+    @IBAction func pressedFPButton() {
+        showAlert(withTitle: "Oops!", andMessage: "Your password is 11 😉")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    private func showAlert(withTitle title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            self.userNameTextField.text = ""
+            self.passwordTextField.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
+
 
--- loginApp/WelcomeViewController.swift
+++ loginApp/WelcomeViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
-class WelcomeViewController: UIViewController {
-
+final class WelcomeViewController: UIViewController {
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var logOutButton: UIButton!
+    
+    var user = ""
+    
+    let gradientWelcomeView = CAGradientLayer()
+    let gradientButtonLogOut = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        welcomeLabel.text = "Welcome, \(user)!"
+        view.layer.insertSublayer(gradientWelcomeView , at: 0)
+        customizingTheAppearanceOfTheButton()
     }
+}
 
+extension WelcomeViewController {
+    func customizingTheAppearanceOfTheButton() {
+        logOutButton.layer.cornerRadius = 5
+        logOutButton.tintColor = .white
+        gradientButtonLogOut.frame = logOutButton.bounds
+        gradientButtonLogOut.colors = [UIColor.red.cgColor, UIColor.systemOrange.cgColor, UIColor.systemYellow.cgColor]
+        gradientButtonLogOut.cornerRadius = 5
+        gradientButtonLogOut.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientButtonLogOut.endPoint = CGPoint(x: 1.3, y: 1.3)
+        logOutButton.layer.insertSublayer(gradientButtonLogOut, at: 0)
+    }
 }
